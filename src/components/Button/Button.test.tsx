9e0453ac890acd2e3d31b718c4e8f3166fc72499
Bohdan Baidan <baidanbv@gmail.com>
2024-03-04 20:27:55 +0100

    deploy app

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+
 import Button from './Button';
 
-describe('<Button />', () => {
-  test('it should mount', () => {
-    render(<Button />);
-    
-    const button = screen.getByTestId('Button');
-
-    expect(button).toBeInTheDocument();
-  });
+test('renders button with provided title', () => {
+  const btnTitle = 'Click me';
+  render(<Button btnTitle={btnTitle} />);
+  const buttonElement = screen.getByTestId('Button');
+  expect(buttonElement).toBeInTheDocument();
+  expect(buttonElement).toHaveTextContent(btnTitle);
 });
